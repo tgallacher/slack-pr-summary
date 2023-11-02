9600b6a3533a7ce365cd22213670395049932abd
Tom Gallacher <tgallacher@users.noreply.github.com>
2023-11-02 14:40:34 +0000

    feat: initial basic flow

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,51 @@
-import * as core from '@actions/core'
-import { wait } from './wait'
+import * as core from "@actions/core";
+import * as github from "@actions/github";
 
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
+interface PRItem {
+  /* PR title  */
+  title: string;
+  /* PR link  */
+  url: string;
+  /* PR author login  */
+  author?: string;
+}
+
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const token: string = core.getInput("github_token");
+    const repoOwner: string = core.getInput("repoOwner");
+    const repoName: string = core.getInput("repoName");
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.debug("Getting authenticated Octo client");
+    const octokit = github.getOctokit(token);
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.debug(`Fetching open PRs for repo: "${repoOwner}/${repoName}"`);
+    const openPRsResponse = await octokit.rest.pulls.list({
+      owner: repoOwner,
+      repo: repoName,
+      state: "open",
+      sort: "created",
+      direction: "desc",
+    });
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    if (openPRsResponse.status >= 400) {
+      const msg = `Failed to fetch PRs, HTTP${openPRsResponse.status}`;
+      return core.setFailed(msg);
+    }
+
+    core.info(`Fetched ${openPRsResponse.data.length} open PRs`);
+
+    const prs: PRItem[] = openPRsResponse.data
+      .filter(p => p.draft !== true)
+      .map(p => ({
+        title: p.title,
+        url: p.url,
+        author: p.user?.login,
+      }));
+
+    core.setOutput("prs", prs);
   } catch (error) {
     // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) core.setFailed(error.message);
   }
 }
