403588ccf8345428a707879476aba4b059330a6d
Tom Gallacher <tgallacher@users.noreply.github.com>
2023-11-02 16:40:06 +0000

    feat: format and post to Slack webhook

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
+import axios, { AxiosError } from "axios";
 
 interface PRItem {
   /* PR title  */
@@ -13,11 +14,21 @@ interface PRItem {
 export async function run(): Promise<void> {
   try {
     const token: string = core.getInput("github_token");
-    const repoOwner: string = core.getInput("repoOwner");
-    const repoName: string = core.getInput("repoName");
+    const rawRepo: string = core.getInput("repo");
+    const slackMsgHeader: string = core.getInput("msg_header");
+    const slackWebhookUrl: string = core.getInput("slack_webhook_url");
 
     core.debug("Getting authenticated Octo client");
     const octokit = github.getOctokit(token);
+
+    const repo = rawRepo.replace(/https?:\/\//, "").replace("github.com/", "");
+    const [repoOwner, repoName] = repo.match(/(.+?)\/(.+)/) || [];
+
+    if (!repoOwner || !repoName) {
+      return core.setFailed(
+        `Failed to parse repo owner and/or name from url, "${rawRepo}"`,
+      );
+    }
 
     core.debug(`Fetching open PRs for repo: "${repoOwner}/${repoName}"`);
     const openPRsResponse = await octokit.rest.pulls.list({
@@ -43,9 +54,45 @@ export async function run(): Promise<void> {
         author: p.user?.login,
       }));
 
-    core.setOutput("prs", prs);
+    const text = prs
+      .map(p => `- <${p.url}|${p.title}>${p.author ? "\t_(p.author)_" : ""}`)
+      .join("\n");
+
+    core.debug("Formatted Slack PR list:");
+    core.debug(text);
+
+    // Slack
+    const message = {
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: slackMsgHeader,
+          },
+        },
+        { type: "divider" },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text,
+          },
+        },
+      ],
+    };
+
+    core.debug("Posting summary to slack webhook..");
+    await axios.post(slackWebhookUrl, message);
   } catch (error) {
+    if (error instanceof AxiosError) {
+      core.error(
+        `Failed to pose data to Slack webhook: "HTTP${error.response?.status} ${error.response?.statusText}"`,
+      );
+      return core.setFailed(error.response?.data);
+    }
+
     // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message);
+    if (error instanceof Error) return core.setFailed(error.message);
   }
 }
