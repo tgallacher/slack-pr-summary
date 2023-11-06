13c5ed20832c5e33ee980eb139e3c8d6528a0641
Tom Gallacher <tgallacher@users.noreply.github.com>
2023-11-06 13:35:07 +0000

    feat: add pr opened time + num comments to output

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
 import axios, { AxiosError } from "axios";
-
+import { intlFormatDistance } from "date-fns";
+import Bottleneck from "bottleneck";
 import { getRepoFragmentsFromUrl } from "./utils";
 
 interface PRItem {
@@ -11,6 +12,80 @@ interface PRItem {
   url: string;
   /* PR author login  */
   author?: string;
+  /* PR number */
+  number: number;
+  /* Number of PR comments */
+  numComments: number | null;
+  /* PR opened: ISO8061 */
+  opened: string;
+}
+
+async function getGithubPRSummary(
+  token: string,
+  repoOwner: string,
+  repoName: string,
+): Promise<PRItem[]> {
+  core.debug("Getting authenticated Octo client");
+  const octokit = github.getOctokit(token);
+  const limiter = new Bottleneck({
+    reservoir: 1000,
+    reservoirRefreshInterval: 60 * 60 * 1000, // 1 hr
+    reservoirRefreshAmount: 1000,
+
+    maxConcurrent: 1,
+    minTime: 50,
+  });
+
+  core.debug(`Fetching open PRs for repo: "${repoOwner}/${repoName}"`);
+  const openPRsResponse = await octokit.rest.pulls.list({
+    owner: repoOwner,
+    repo: repoName,
+    state: "open",
+    sort: "created",
+    direction: "desc",
+  });
+
+  if (openPRsResponse.status >= 400) {
+    const msg = `Failed to fetch PRs, HTTP${openPRsResponse.status}`;
+    core.setFailed(msg);
+    process.exit(1);
+  }
+  core.info(`Fetched ${openPRsResponse.data.length} open PRs`);
+
+  const prs: PRItem[] = openPRsResponse.data
+    .filter(p => p.draft !== true)
+    .map(p => ({
+      title: p.title,
+      url: p.html_url,
+      author: p.user?.login,
+      opened: p.created_at,
+      number: p.number,
+      numComments: null,
+    }));
+
+  const prsData: PRItem[] = await Promise.all(
+    prs.map(async p =>
+      limiter.schedule(async () => {
+        const resp = await octokit.rest.pulls.get({
+          owner: repoOwner,
+          repo: repoName,
+          pull_number: p.number,
+        });
+
+        if (resp.status >= 400) return p;
+
+        return {
+          ...p,
+          numComments: resp.data.comments,
+        };
+      }),
+    ),
+  );
+
+  // allow the limiter to be garbage collected
+  limiter.disconnect();
+
+  return prsData;
 }
 
 export async function run(): Promise<void> {
@@ -20,9 +95,6 @@ export async function run(): Promise<void> {
     const slackMsgHeader: string = core.getInput("msg_header");
     const slackWebhookUrl: string = core.getInput("slack_webhook_url");
 
-    core.debug("Getting authenticated Octo client");
-    const octokit = github.getOctokit(token);
-
     const { repoOwner, repoName } = getRepoFragmentsFromUrl(repoUrl);
     if (!repoOwner || !repoName) {
       return core.setFailed(
@@ -30,32 +102,21 @@ export async function run(): Promise<void> {
       );
     }
 
-    core.debug(`Fetching open PRs for repo: "${repoOwner}/${repoName}"`);
-    const openPRsResponse = await octokit.rest.pulls.list({
-      owner: repoOwner,
-      repo: repoName,
-      state: "open",
-      sort: "created",
-      direction: "desc",
-    });
+    const prs = await getGithubPRSummary(token, repoOwner, repoName);
 
-    if (openPRsResponse.status >= 400) {
-      const msg = `Failed to fetch PRs, HTTP${openPRsResponse.status}`;
-      return core.setFailed(msg);
-    }
-
-    core.info(`Fetched ${openPRsResponse.data.length} open PRs`);
-
-    const prs: PRItem[] = openPRsResponse.data
-      .filter(p => p.draft !== true)
-      .map(p => ({
-        title: p.title,
-        url: p.html_url,
-        author: p.user?.login,
-      }));
+    const authorTxt = (pr?: PRItem): string => (pr?.author ? pr.author : "");
+    const commentsTxt = (pr: PRItem): string =>
+      pr.numComments ? `${pr.numComments} comments` : "";
+    const openedTxt = (pr: PRItem): string =>
+      pr.opened ? `${intlFormatDistance(new Date(pr.opened), new Date())}` : "";
 
     const text = prs
-      .map(p => `- <${p.url}|${p.title}>${p.author ? `\t_(${p.author})_` : ""}`)
+      .map(
+        p =>
+          `- <${p.url}|${p.title}>\t_${openedTxt(p)}, ${commentsTxt(
+            p,
+          )}, ${authorTxt(p)}_`,
+      )
       .join("\n");
 
     core.debug("Formatted Slack PR list:");
